edec881aa863f0454806f10cbe86f906d0f008ee
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-18 18:56:22 -0400

    router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -12,11 +13,13 @@ const errorMiddleware = require('./middleware/error-handler');
 // middleware
 app.use(express.json());
 
-// rootes
+// routes
 
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use('/api/v1/products', productsRouter);
 
 // products route
 
