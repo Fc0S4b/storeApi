b0a29b180ce7e6fe4f46ba2cc9c70887c81b8051
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-19 02:56:38 -0400

    select option

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,12 +21,17 @@ const getAllProducts = async (req, res) => {
   // console.log(req.query);
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  // sort
   if (sort) {
     // console.log(sort);
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createAt');
+  }
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
