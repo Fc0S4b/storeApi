f660709d1ce75f81a41987b43dd67ab152223ff3
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-19 02:51:26 -0400

    sort getAllProducts implementation

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -19,8 +19,16 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
   // console.log(req.query);
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    // console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createAt');
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
