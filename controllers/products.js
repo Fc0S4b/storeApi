90c3b06ed1f03153de1c0f6e5456d88bcdba500b
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-20 17:43:51 -0400

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,8 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort('name')
-    .select('name price')
-    .limit(30)
-    .skip(1);
+  const products = await Product.find({}).sort('name').select('name price');
+
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
@@ -37,6 +34,14 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  // 23 => 23/4 límite de 7
+  //   4 páginas de 7 7 7 2
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
