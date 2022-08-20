16e40380e20cf01b87741936345677f0cae93238
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-20 17:47:28 -0400

    numeric filters setup

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,10 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name').select('name price');
-
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price')
+    .select('name price');
+  //gt es mayor que, greater than
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
